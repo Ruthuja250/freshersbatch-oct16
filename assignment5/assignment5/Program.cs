c34a8016c4cc75591f250a0e211f1df548f28d92
Ruthuja250 <95559469+Ruthuja250@users.noreply.github.com>
2022-10-31 21:28:11 +0530

    Add files via upload

---
+++ assignment5/assignment5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pie = 3.14;
+            double r;
+            Console.WriteLine("enter the radius");
+            r = Convert.ToDouble(Console.ReadLine());
+            double cirumference = 2 * pie * r;
+            double area = pie * r * r;
+            Console.WriteLine("circumference of circle"+cirumference);
+            Console.WriteLine("area of circle"+area);
+            Console.ReadLine();
+        }
+    }
+}
